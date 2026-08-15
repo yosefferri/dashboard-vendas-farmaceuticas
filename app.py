35d2b8011c471d09ddcd3ced53c52ba330f328b9
yoseferri <joseferri225@gmail.com>
2026-08-15 00:36:05 -0300

    layout responsivo

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ components.html("""
 function traduzirTextos() {
     const parentDoc = window.parent.document;
     parentDoc.querySelectorAll('div').forEach(el => {
-        if (el.textContent === 'Choose options' && el.children.length === 0) {
+        if (el.textContent === ' ' && el.children.length === 0) {
             el.textContent = 'Selecione as cidades';
         }
         if (el.textContent === 'Select all' && el.children.length === 0) {
